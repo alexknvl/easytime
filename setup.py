17f529977f6b76d4f1cc80f9e5d605ebd83d64fe
Alexander Konovalov <alex.knvl@gmail.com>
2015-10-20 13:45:59 -0400

    First commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='easytime',
+      version='0.1',
+      description='A less error-prone date&time manipulation library',
+      author='Alexander Konovalov',
+      author_email='alex.knvl@gmail.com',
+      packages=['easytime'])
