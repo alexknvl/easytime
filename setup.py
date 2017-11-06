ac1291dc6fea56683fb8c73ab6f7d8cb0276e645
Alexander Konovalov <alex.knvl@gmail.com>
2017-11-05 20:50:15 -0500

    Changed the module name due to a conflict with easytime on pypi.

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@
 
 from distutils.core import setup
 
-setup(name='easytime',
-      version='0.1',
+setup(name='osunlp-easytime',
+      version='0.2',
       description='A less error-prone date&time manipulation library',
       author='Alexander Konovalov',
       author_email='alex.knvl@gmail.com',
-      packages=['easytime'])
+      packages=['easytime'],
+      install_requires=[
+          'pytz',
+          'python-dateutil'
+      ])
